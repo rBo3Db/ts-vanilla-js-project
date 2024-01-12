6e7c009563c939113d0a698e3ade1c1f2bfe7d93
alexey_chernitsyn <chernitsyn@gnivc.ru>
2024-01-13 01:16:04 +0400

    add logic for change bg

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,42 @@
 import "./styles.css";
 
 // console.log(styles);
-const main = document.getElementById('main');
+const body = document.getElementById("body");
 
+// console.log(body);
+
+body?.addEventListener("click", (e) => {
+  const classesOfBody = body?.classList;
+  const target = (e.target as Element).id;
+  console.log(classesOfBody, '----------')
+  console.log(target, '----------')
+  if (
+    (target === "summer-button" || "summer-image") &&
+    !classesOfBody?.contains("summer")
+  ) {
+    // classesOfBody?.contains("summer");
+    classesOfBody?.remove("winter");
+    classesOfBody?.remove("rainy");
+    classesOfBody?.add("summer");
+  } else if (
+    (target === "rainy-button" || "rainy-image") &&
+    !classesOfBody?.contains("rainy")
+  ) {
+    // classesOfBody?.contains("summer");
+    classesOfBody?.remove("winter");
+    classesOfBody?.remove("summer");
+    classesOfBody?.add("rainy");
+  } else if (
+    (target === "winter-button" || "winter-image") &&
+    !classesOfBody?.contains("winter")
+  ) {
+    // classesOfBody?.contains("summer");
+    classesOfBody?.remove("summer");
+    classesOfBody?.remove("rainy");
+    classesOfBody?.add("winter");
+  }
+  e.stopPropagation();
+});
 
 // main?.classList.add(
 //   "first"
